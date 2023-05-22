f013008f5aa28fa44815848aea2cce253b639f07
Owen Arts <owen.arts@hotmail.com>
2023-05-22 03:39:14 +0200

    camera movement around object

--- Opdrachten3D/Camera.cpp
+++ Opdrachten3D/Camera.cpp
@@ -19,7 +19,30 @@ glm::mat4 Camera::getMatrix()
 	glm::mat4 ret(1.0f);
 	ret = glm::rotate(ret, rotation.x, glm::vec3(1, 0, 0));
 	ret = glm::rotate(ret, rotation.y, glm::vec3(0, 1, 0));
-	ret = glm::translate(ret, position);
+	//ret = glm::translate(ret, position + glm::vec3(0,0,-distanceToObject));
+	//return ret;
+
+	// Calculate the camera position based on the rotation, distance, and the player's position
+	glm::vec3 cameraPosition;
+
+	if (activePlayer)
+	{
+		glm::vec3 playerTwoPosition = playerTwo->getPosition() + glm::vec3(0, .5f, 0);
+		glm::vec3 offset(0, 0, distanceToObject);
+		glm::mat4 rotationMatrix = glm::rotate(glm::mat4(1.0f), rotation.y, glm::vec3(0, 1, 0));
+		glm::vec4 rotatedOffset = rotationMatrix * glm::vec4(offset, 1.0f);
+		cameraPosition = playerTwoPosition + glm::vec3(rotatedOffset);
+	}
+	else
+	{
+		glm::vec3 playerOnePosition = playerOne->getPosition() + glm::vec3(0, .5f, 0);
+		glm::vec3 offset(0, 0, distanceToObject);
+		glm::mat4 rotationMatrix = glm::rotate(glm::mat4(1.0f), -rotation.y, glm::vec3(0, 1, 0));
+		glm::vec4 rotatedOffset = rotationMatrix * glm::vec4(offset, 1.0f);
+		cameraPosition = playerOnePosition + glm::vec3(rotatedOffset);
+	}
+
+	ret = glm::translate(ret, -cameraPosition);
 	return ret;
 }
 
@@ -32,17 +55,7 @@ void Camera::move(float angle, float fac)
 
 void Camera::update(GLFWwindow* window, bool activePlayer)
 {
-	if (activePlayer)
-	{
-		position.x = -playerTwo->getPosition().x;
-		position.z = -playerTwo->getPosition().z;
-	}
-	else
-	{
-		position.x = -playerOne->getPosition().x;
-		position.z = -playerOne->getPosition().z;
-	}
-
+	this->activePlayer = activePlayer;
 	double x, y;
 	glfwGetCursorPos(window, &x, &y);
 
@@ -55,10 +68,22 @@ void Camera::update(GLFWwindow* window, bool activePlayer)
 
 	rotation.y -= (float)(lastX - x) / 100.0f;
 
-	lastX = x;
-	//lastY = y;
+	//std::cout << rotation.y << std::endl;
 
-	// Adds the movement via awsd
+	lastX = x;/*
+	if (activePlayer)
+	{
+		position.x = -playerTwo->getPosition().x;
+		position.z = -playerTwo->getPosition().z;
+	}
+	else
+	{
+		position.x = -playerOne->getPosition().x;
+		position.z = -playerOne->getPosition().z;
+	}*/
+
+
+	// Adds the movement via awsd - REMOVED
 	/*
 	if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
 		move(0, 0.05f);
